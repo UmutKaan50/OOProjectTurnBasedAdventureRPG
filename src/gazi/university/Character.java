9896b3d856f961d5c609a15a5f2709e61487468d
Umut Kaan <74989036+umutaan50@users.noreply.github.com>
2021-12-31 18:30:20 +0300

    Made some changes in character class.

--- src/gazi/university/Character.java
+++ src/gazi/university/Character.java
@@ -8,6 +8,51 @@ public class Character {
     private int money;
     private Inventory inventory;
 
+    // Added variables
+    // Dexterity and intelligence was forgotten.
+    private int dexterity;
+    private int intelligence;
+    private int lvl = 0;
+    private int experience = 0;
+
+    private static final int xpToLvl2 = 50;
+    private static final int xpToLvl3 = 150;
+    private static final int xpToLvl4 = 350;
+    private static final int xpToLvl5 = 650;
+
+    // Getters and setters for my additional variables.
+    public int getIntelligence() {
+        return intelligence;
+    }
+
+    public void setIntelligence(int intelligence) {
+        this.intelligence = intelligence;
+    }
+
+    public int getLvl() {
+        return lvl;
+    }
+
+    public void setLvl(int lvl) {
+        this.lvl = lvl;
+    }
+
+    public int getExperience() {
+        return experience;
+    }
+
+    public void setExperience(int experience) {
+        this.experience = experience;
+    }
+
+    public int getDexterity() {
+        return dexterity;
+    }
+
+    public void setDexterity(int dexterity) {
+        this.dexterity = dexterity;
+    }
+
     public Character(String name, int health, int mana, int strength, int money, Inventory inv){
         this.name = name;
         this.health = health;
@@ -21,9 +66,40 @@ public class Character {
         // In addition I think it'll be better to have if and else if cases for leveling up.
         // I'll be commiting and pushing them soon inshaallah.
 
-        // Deleted something here.
+        // Checking the level up process here.
+        if (experience >= xpToLvl2 && lvl == 1)
+        {
+            lvl++;
+            experience = 0;
+        }
+        else if (experience >= xpToLvl3 && lvl == 2)
+        {
+            lvl++;
+            experience = 0;
+        }
+        else if (experience >= xpToLvl4 && lvl == 3)
+        {
+            lvl++;
+            experience = 0;
+        }
+        else if(experience >= xpToLvl5 && lvl == 4)
+        {
+            lvl++;
+            experience = 0;
+        }
+
+
+
+
 
     }
+    private void levelUpChanges(){
+        health += 20;
+        mana += 20;
+        strength += 5;
+        money += 100;
+    }
+
     public void buyItem(){
 
     }
